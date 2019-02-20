773e21da8cdb83404ab0f246e2ef3aea6fb2bed2
Andris Reinman <andris@kreata.ee>
2019-02-20 12:18:11 +0200

    reject configurable rspamd keys

--- index.js
+++ index.js
@@ -830,7 +830,7 @@ exports.hook_queue = function(next, connection) {
     plugin.loginfo('BLRES all=' + JSON.stringify(plugin.rspamd.blacklist) + ' bl=' + JSON.stringify(blacklisted), plugin, connection);
     if (blacklisted) {
         //return next(DENY, plugin.dsnSpamResponse(connection, blacklisted.key));
-        return next(DENY, DSN.rcpt_too_fast());
+        return next(DENY, DSN.addr_unspecified());
     }
 
     // results about verification (TLS, SPF, DKIM)
