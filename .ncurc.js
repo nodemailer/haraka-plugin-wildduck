6b93f19a5eb3b189c92170138bf5bc8f6ec1a762
Andris Reinman <andris@reinman.eu>
2024-04-19 12:53:32 +0300

    fix(eslint): Lock eslint version to v8

--- .ncurc.js
+++ .ncurc.js
@@ -2,6 +2,9 @@ module.exports = {
     upgrade: true,
     reject: [
         // mongodb 5.x driver does not support callbacks, only promises
-        'mongodb'
+        'mongodb',
+
+        // some api changes, need to address in the future
+        'eslint'
     ]
 };
